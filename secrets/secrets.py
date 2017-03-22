3b2b514f6fc7ca65248377313a887f27a64c7ec9
John Atherton <john-atherton@Johns-MacBook-Pro-2.local>
2017-03-22 13:00:05 -0700

    removed API keys

--- secrets/secrets.py
+++ secrets/secrets.py
@@ -1,4 +1,6 @@
-access_token = '804100625101262848-kbcCQvZzFLn1GutHZwcpvFNJxvtHVxX'
-access_token_secret = "JpIgOMyd2ukxDnkZ0Q78WWL9xfZwMbENdzKKdd8CcNERs"
-consumer_key = "8TmuknpicnmWqm1hyTnaPIP8R"
-consumer_secret = "lP4wkNAgvth5P0GieFjHGgISpllrMsU8zr9LTWKWeY0QpDZyrp"
+# go to https://dev.twitter.com/rest/public to get your own keys
+
+access_token = ''
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
